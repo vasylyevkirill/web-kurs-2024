8a1be7b8387962eddb4c80eb77554b89bbf89049
Кирилл Васильев <vasylyev@artlebedev.ru>
2024-11-15 13:17:04 +0300

    backend register project models

--- taxi/admin.py
+++ taxi/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from taxi.models import Car, City, District, Street, Address, Driver, Consumer, Ride, DriverRate, ConsumerRate, RideAddressesQueue
 
-# Register your models here.
+
+admin.site.register(Car)
+admin.site.register(City)
+admin.site.register(District)
+admin.site.register(Street)
+admin.site.register(Address)
+admin.site.register(Driver)
+admin.site.register(Consumer)
+admin.site.register(Ride)
+admin.site.register(DriverRate)
+admin.site.register(ConsumerRate)
+admin.site.register(RideAddressesQueue)
