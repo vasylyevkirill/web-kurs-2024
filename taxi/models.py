1d5b68c8f190195a63a44a6b97fc1e9b277a47e3
Кирилл Васильев <vasylyev@artlebedev.ru>
2024-11-15 10:03:06 +0300

    taxi database init

---
+++ taxi/models.py
@@ -0,0 +1,201 @@
+import datetime
+
+from django.db import models
+from django.conf import settings
+from django.core import validators
+
+
+class Car(models.Model):
+    class CarComfortClass(models.TextChoices):
+        COMFORT_PLUS = 'comfort_plus', 'Comfort Plus'
+        BUSINESS = 'business', 'Business'
+        TOGETHER = 'together', 'Together'
+        COMFORT = 'comfort', 'Comfort'
+        ECONOM = 'econom', 'Econom'
+
+    number = models.CharField('Car Number', max_length=10, primary_key=True)
+    color = models.CharField('Color', max_length=63, blank=False, null=False)
+    make = models.CharField('Make', max_length=100, blank=False, null=False)
+    series = models.CharField('Series', max_length=100, blank=False, null=False)
+
+    is_ready = models.BooleanField('Ready to use', default=True, blank=False, null=False)
+    comfort_class = models.CharField(
+        'Comfort class', max_length=50, choices=CarComfortClass.choices
+    )
+
+    def __str__(self):
+        return f'{self.number}: {self.color} {self.make} {self.series}'
+
+
+class City(models.Model):
+    name = models.CharField('City', max_length=511, null=False, blank=False)
+
+
+class District(models.Model):
+    models.ForeignKey(
+        City,
+        null=False,
+        blank=False,
+        related_name='items',
+        on_delete=models.CASCADE
+    )
+    name = models.CharField('District', max_length=511, null=False, blank=False)
+
+
+class Street(models.Model):
+    models.ForeignKey(
+        District,
+        null=False,
+        blank=False,
+        related_name='items',
+        on_delete=models.CASCADE
+    )
+    name = models.CharField('Street name', max_length=511, null=False, blank=False)
+
+
+class Address(models.Model):
+    models.ForeignKey(
+        Street,
+        null=False,
+        blank=False,
+        related_name='items',
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField('Address name', max_length=10, null=False, blank=False)
+
+
+class Driver(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+    current_car = models.OneToOneField(
+        Car,
+        on_delete=models.PROTECT,
+        null=True,
+    )
+
+    location = models.ForeignKey(District, on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return f'Driver: {self.user}'
+        
+
+class Consumer(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+    def __str__(self):
+        return f'Consumer: {self.user}'
+    
+
+class Ride(models.Model):
+    consumer = models.ForeignKey(
+        Consumer,
+        on_delete=models.PROTECT,
+        related_name='rides'
+    )
+    driver = models.ForeignKey(
+        Driver,
+        on_delete=models.PROTECT,
+        related_name='rides',
+        null=True
+    )
+    date_created = models.DateTimeField(default=datetime.datetime.now)
+    date_ended = models.DateTimeField(default=None, null=True)
+
+    class Meta:
+        unique_together = (('consumer', 'driver', 'date_created'),)
+        ordering = 'date_created '.split()
+ 
+    @property
+    def price(self):
+        pass
+    @property
+    def status(self):
+        pass
+
+
+class Rate():
+    ride = models.ForeignKey(
+        Ride,
+        on_delete=models.PROTECT,
+        related_name='rates'
+    )
+    comment = models.TextField('Comment', blank=False, null=False)
+    rate = models.PositiveIntegerField('Rate',  validators.MaxValueValidator(5))
+
+
+class DriverRate(models.Model, Rate):
+    author = models.ForeignKey(
+        Consumer,
+        on_delete=models.PROTECT,
+        related_name='rates_authored'
+    )
+    target = models.ForeignKey(
+        Driver,
+        on_delete=models.PROTECT,
+        related_name='rates'
+    )
+
+
+class ConsumerRate(models.Model, Rate):
+    target = models.ForeignKey(
+        Consumer,
+        on_delete=models.PROTECT,
+        related_name='rates'
+    )
+    author = models.ForeignKey(
+        Driver,
+        on_delete=models.PROTECT,
+        related_name='rates_authored'
+    )
+
+
+class RideAddressesQueue(models.Model):
+    ride = models.ForeignKey(
+        Ride,
+        on_delete=models.CASCADE,
+        blank=False,
+        related_name='adresses',
+    )
+    address = models.ForeignKey(
+        Address,
+        on_delete=models.CASCADE,
+        blank=False,
+        related_name='rides',
+    )
+    order = models.PositiveIntegerField('Order', default=0)
+    date_created = models.DateTimeField(default=datetime.datetime.now)
+    date_ended = models.DateTimeField(default=None, null=True)
+
+    def __str__(self):
+        return f'{self.order + 1}. {self.ride}'
+
+    def save(self, *args, force_insert=False, **kwargs):
+        if force_insert:
+            default_order = SubjectScheduleItemQueue.objects.filter(subject_item=self.subject_item).count()
+            self.order = default_order
+        super().save(*args, **kwargs)
+
+    class Meta:
+        unique_together = (('ride', 'address'),)
+        ordering = 'order date_ended date_created'.split()
+
+
+def on_delete_queue(instance, **kwargs):
+    for queue_record in RideAddressesQueue.objects.filter(ride = instance.ride, order__gt = instance.order):
+        queue_record.order -= 1
+        queue_record.save(force_update=True)
+    
+
+models.signals.post_delete.connect(
+    on_delete_queue,
+    sender=RideAddressesQueue,
+    dispatch_uid='on_delete_queue'
+)
